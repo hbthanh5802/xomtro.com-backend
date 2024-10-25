97099642f0cd4ccf6e790bc56551a73a3672b0d4
ThanK <hbthanh582002@gmail.com>
2024-10-25 07:59:11 +0700

    feat/base: Build code base

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { corsOptions } from '@/configs/cors.config';
+import { checkDatabaseConnection } from '@/configs/database.config';
+import { env } from '@/configs/env.config';
+import { startCronJobs } from '@/crons/jobs';
+import { errorHandler } from '@/middlewares/errorHandler.middleware';
+import { useRoutes } from '@/routes/index.route';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+// Middlewares
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('tiny'));
+
+// Router
+useRoutes(app);
+
+// Error Middleware
+app.use(errorHandler);
+
+const port = env.PORT;
+
+async function start() {
+  try {
+    await checkDatabaseConnection();
+    startCronJobs();
+    app.listen(port, () => {
+      console.log(`[INFO] Server is running at port ${port}`);
+    });
+  } catch (error) {
+    console.log('[ERROR ❌] Failed to run server', error);
+    process.exit(1);
+  }
+}
+
+start();
