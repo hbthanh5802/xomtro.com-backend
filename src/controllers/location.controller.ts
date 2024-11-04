ecd0ba1ffd52e694db4104510437c50212964392
ThanK <hbthanh582002@gmail.com>
2024-11-04 22:10:41 +0700

    feat/user/address: Build more location api

--- src/controllers/location.controller.ts
+++ src/controllers/location.controller.ts
@@ -1,10 +1,21 @@
 import axiosRequest from '@/configs/axiosClient.config';
 import {
-  districtResponseType,
+  distanceMatrixByGoong,
+  distanceMatrixRequestPayload,
+  geocodingByDistanceMatrix,
+  geocodingByGeocodeMap,
+  geocodingReverseByGoong,
+  locationAutoCompleteByGoong,
+  locationAutoCompleteRequestPayload
+} from '@/services/location.service';
+import {
+  autoCompleteResponseType,
+  distanceResponseType,
   getDistrictsListType,
   getWardListType,
   locationResponseType,
-  searchDivisionType
+  searchDivisionType,
+  vehicleType
 } from '@/types/location.type';
 import ApiError from '@/utils/ApiError.helper';
 import { ApiResponse } from '@/utils/ApiResponse.helper';
@@ -95,6 +106,122 @@ export const searchLocation = async (req: Request, res: Response, next: NextFunc
       }
     });
     return new ApiResponse(StatusCodes.OK, ReasonPhrases.OK, searchResult).send(res);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getGeocodingFromAddress = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { address } = req.query;
+    if (!address) {
+      throw new ApiError(StatusCodes.BAD_REQUEST, `"address" parameter is required.`);
+    }
+
+    const apiServices = [
+      () => geocodingByDistanceMatrix(address as string),
+      () => geocodingByGeocodeMap(address as string)
+    ];
+
+    const randomApiServiceIndex = Math.floor(Math.random() * apiServices.length);
+    const response = await apiServices[randomApiServiceIndex]();
+
+    return new ApiResponse(StatusCodes.OK, ReasonPhrases.OK, {
+      ...response,
+      apiIndex: randomApiServiceIndex
+    }).send(res);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getGeocodingReverseFromDimension = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { longitude, latitude } = req.query;
+    if (!longitude || !latitude) {
+      throw new ApiError(StatusCodes.BAD_REQUEST, 'Dimension of location is required');
+    }
+
+    const geocdingReverseResult = await geocodingReverseByGoong(Number(latitude), Number(longitude));
+
+    return new ApiResponse(StatusCodes.OK, ReasonPhrases.OK, geocdingReverseResult).send(res);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAutoCompleteFromSearchValue = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { searchValue, longitude, latitude, limit, radius } = req.query;
+    if (!searchValue) {
+      throw new ApiError(StatusCodes.BAD_REQUEST, 'searchValue is required');
+    }
+
+    const searchPayload: locationAutoCompleteRequestPayload = {
+      searchValue: searchValue as string,
+      limit: Number(limit),
+      location: {
+        longitude: Number(longitude),
+        latitude: Number(latitude)
+      },
+      radius: Number(radius)
+    };
+    const result = await locationAutoCompleteByGoong(searchPayload);
+    const { predictions } = result;
+    const responseData: autoCompleteResponseType[] = predictions.map((prediction) => {
+      const { description, place_id, reference, compound, terms, structured_formatting } = prediction;
+      return {
+        description: description,
+        placeId: place_id,
+        googleMapReference: reference,
+        province: compound.province,
+        district: compound.district,
+        ward: compound.commune,
+        matches: terms,
+        mainText: structured_formatting.main_text,
+        subText: structured_formatting.secondary_text
+      };
+    });
+
+    return new ApiResponse(StatusCodes.OK, ReasonPhrases.OK, responseData).send(res);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getDistanceMatrixFromDimensions = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { origins, destinations, vehicle } = req.query;
+    if (!origins || (Array.isArray(origins) && !origins.length)) {
+      throw new ApiError(
+        StatusCodes.BAD_REQUEST,
+        'Origins value is invalid, must be string<latitude,longitude>|string array same format'
+      );
+    }
+
+    if (!destinations || (Array.isArray(destinations) && !destinations.length)) {
+      throw new ApiError(
+        StatusCodes.BAD_REQUEST,
+        'Destinations value is invalid, must be string<latitude,longitude>|string array same format'
+      );
+    }
+
+    if (vehicle && !Object.values(vehicleType).includes(vehicle as vehicleType)) {
+      throw new ApiError(StatusCodes.BAD_REQUEST, 'Vehicle value is invalid, can be  |car |truck |taxi |bike |hd');
+    }
+
+    const requestPayload: distanceMatrixRequestPayload = {
+      origins: Array.isArray(origins) ? origins.join(' | ') : (origins as string),
+      destinations: Array.isArray(destinations) ? destinations.join(' | ') : (destinations as string),
+      ...(vehicle && { vehicle: vehicle as string })
+    };
+    const response = await distanceMatrixByGoong(requestPayload);
+    const responseData: distanceResponseType[] = response.rows[0].elements.map((distanceData) => {
+      const { distance, duration } = distanceData;
+      return { distance, duration };
+    });
+
+    return new ApiResponse(StatusCodes.OK, ReasonPhrases.OK, responseData).send(res);
   } catch (error) {
     next(error);
   }
