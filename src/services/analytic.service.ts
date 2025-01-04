b645bbf2ee697a9f1fba89e62e8bba0670e3822b
ThanK <hbthanh582002@gmail.com>
2025-01-05 02:11:00 +0700

    feat/analytics: Build analytic feature

--- src/services/analytic.service.ts
+++ src/services/analytic.service.ts
@@ -1,9 +1,9 @@
 import { db } from '@/configs/database.config';
-import { posts } from '@/models/schema';
+import { joinPosts, posts, rentalPosts, wantedPosts } from '@/models/schema';
 import { ConditionsType } from '@/types/drizzle.type';
 import { PostSelectSchemaType } from '@/types/schema.type';
 import { customCount, processCondition } from '@/utils/schema.helper';
-import { and, desc } from 'drizzle-orm';
+import { and, desc, eq, gte, lte, sql } from 'drizzle-orm';
 
 // POSTS
 export const selectPostsCountByTypeWithPostConditions = async <T extends PostSelectSchemaType>(
@@ -26,6 +26,57 @@ export const selectPostsCountByTypeWithPostConditions = async <T extends PostSel
     query = query.where(and(...whereClause)).$dynamic();
   }
   query = query.groupBy(posts.type).orderBy(desc(customCount(posts.type)));
+
+  return query;
+};
+
+export type SelectPostAnalyticConditionType = {
+  totalAreaStart?: number;
+  totalAreaEnd?: number;
+  year: number;
+  provinceName?: string;
+  districtName?: string;
+  wardName?: string;
+  type: 'rental' | 'wanted' | 'join';
+};
+export const selectRentalPostAnalyticByConditions = async (conditions: SelectPostAnalyticConditionType) => {
+  const detailTableInstance =
+    conditions.type === 'rental' ? rentalPosts : conditions.type === 'wanted' ? wantedPosts : joinPosts;
+  let query = db
+    .select({
+      month: sql<number>`MONTH(${posts.updatedAt})`,
+      avgPrice: sql<number>`ROUND(AVG((${detailTableInstance.priceStart}+${detailTableInstance.priceEnd})/2),-3)`
+    })
+    .from(posts)
+    .leftJoin(detailTableInstance, eq(posts.id, detailTableInstance.postId))
+    .$dynamic();
+
+  const whereConditions = [];
+  if (conditions.totalAreaStart) {
+    whereConditions.push(gte(detailTableInstance.totalArea, conditions.totalAreaStart));
+  }
+  if (conditions.totalAreaEnd) {
+    whereConditions.push(lte(detailTableInstance.totalArea, conditions.totalAreaEnd));
+  }
+  if (conditions.provinceName) {
+    whereConditions.push(eq(posts.addressProvince, conditions.provinceName));
+  }
+  if (conditions.districtName) {
+    whereConditions.push(eq(posts.addressDistrict, conditions.districtName));
+  }
+  if (conditions.wardName) {
+    whereConditions.push(eq(posts.addressWard, conditions.wardName));
+  }
+  if (conditions.year) {
+    whereConditions.push(sql`YEAR(${posts.updatedAt})=${conditions.year}`);
+  }
+  if (whereConditions.length) {
+    query = query.where(and(...whereConditions)).$dynamic();
+  }
+
+  query = query.groupBy(sql`MONTH(${posts.updatedAt})`).orderBy(sql`month(${posts.updatedAt})`);
+
+  console.log(query.toSQL());
 
   return query;
 };
